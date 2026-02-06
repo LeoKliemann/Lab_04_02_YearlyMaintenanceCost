444e177d45163f0a4dfae94c6dc900a3efd5b55a
LeoKliemann <lgkliemann05@gmail.com>
2026-02-06 16:11:51 -0500

    Declarations done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main{
     public static void main(String[] args)
     {
+        //Declarations
+        double winter = 9321.48;
+        double spring = 1648.32;
+        double summer = 9324.47;
+        double autumn = 3782.16;
+        double yearlyTotalCost;
 
     }
 }
